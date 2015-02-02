cfbe0ceef739a2dfbe82097e1904b9bf7b65e626
Finbar Ryan <finbar_ryan@hotmail.com>
2015-02-02 18:03:03 +0000

    MVC Application

---
+++ ContosoAirMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ContosoAirMVC.MvcApplication" Language="C#" %>
---
+++ ContosoAirMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ContosoAirMVC.Startup))]
+namespace ContosoAirMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
